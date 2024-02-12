faeca2fbe126aa9ffeb08f7e64827a19c4ca64dc
lberg001 <156349649+lberg001@users.noreply.github.com>
2024-02-12 15:09:30 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,53 @@
+const socket = io();
 let radius = 5;
 
 function setup() {
-  createCanvas(400, 400);
+  if (windowWidth > windowHeight) {
+    canvas = createCanvas(windowWidth, windowWidth);
+  } else {
+    canvas = createCanvas(windowHeight, windowHeight);
+  }
+  canvas = createCanvas(windowWidth, windowHeight);
+  background(220);
 }
 
-function draw() {
-  background(220);
-  fill(255, 0, 0);
-  circle(width / 2, height / 2, 50);
+function draw() {}
 
-  if (MouseIsPressed) {
-    circle(mouseX, mouseY, radius);
-    // Hello can you see this?
+function mouseDragged() {
+  console.log(mouseX + "," + mouseY);
+  fill(0);
+  noStroke();
+  circle(mouseX, mouseY, radius);
+  let data = {
+    x: mouseX,
+    y: mouseY,
+  };
+
+  // send the mouse data to the server by using name "mouse"
+  socket.emit("mouse", data);
+}
+
+socket.on("drawing", (data) => {
+  console.log(data);
+
+  onDrawingEvent(data);
+});
+
+function onDrawingEvent(data) {
+  noStroke();
+  fill("#6358cc");
+  circle(data.x, data.y, radius);
+}
+
+function windowResized() {
+  //wipes out the history of drawing if resized, potential fix, draw to offscreen buffer
+  //https://p5js.org/reference/#/p5/createGraphics
+  resizeCanvas(windowWidth, windowHeight);
+  //ratio fix... but then need to make a bunch of other UX decisions like whether you zoom into the canvas or center it somehow
+  if (windowWidth > windowHeight) {
+    resizeCanvas(windowWidth, windowWidth);
+  } else {
+    resizeCanvas(windowHeight, windowHeight);
   }
+  background(220);
 }
