de32bbb74c62352e5ed664e6fa325beeb3f986af
Fan031 <fange.jjia@gmail.com>
2024-02-18 14:44:43 +0000

    server update

--- app.js
+++ app.js
@@ -8,10 +8,18 @@ const io = new Server(server);
 // Tell our Node.js Server to host our P5.JS sketch from the public folder.
 app.use(express.static("public"));
 
+const port = process.env.PORT;
+
 // Setup Our Node.js server to listen to connections from chrome, and open chrome when it is ready
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+server.listen(port, () => {
+  console.log(`starting server at ${port}`);
 });
+
+
+// Setup Our Node.js server to listen to connections from chrome, and open chrome when it is ready
+//server.listen(3000, () => {
+//  console.log("listening on *:3000");
+//});
 
 io.on("connection", (socket) => {
   console.log("a user connected : " + socket.id);
